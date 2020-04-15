aaf07c716955ddea7149004830e7ef48cee34d58
梁朝伟s <1500107668@qq.com>
2020-04-15 12:47:06 +0800

    第一次提交

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//实验2,1
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
